379c477a883ee1764379013a46671518d041983b
SKGEzhil <skgezhil2005@gmail.com>
2021-04-15 14:31:06 +0530

    updated requirements.txt

--- SKGEzhil_Voice_Assistant/script/database.py
+++ SKGEzhil_Voice_Assistant/script/database.py
@@ -5,7 +5,7 @@ from SKGEzhil_Voice_Assistant.script import config
 db_connection = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="EzHiL2005",
+    passwd=config.mysql_password,
     database="assistant_database"
 )
 print(db_connection)
