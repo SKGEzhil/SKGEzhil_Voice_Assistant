aabae58839162ae17cc32b7496c8376de9ac0503
SKGEzhil <skgezhil2005@gmail.com>
2021-04-18 12:04:11 +0530

    added license

---
+++ SKGEzhil_Voice_Assistant/script/config(template).py
@@ -0,0 +1,13 @@
+custom_search_api = 'AIzaSyB2CHrIXCerEertGu1bLUxmZgVBq-gqCH4'
+custom_search_id = '015295736575808137604:x3cf3qjl5fs'
+news_api = '7dfb535cf0c44d3c99e73ec00a2d8152'
+weather_api = '8ef61edcf1c576d65d836254e11ea420'
+wolframalpha_api = 'R2K75H-7ELALHR35X'
+gmail_id = 'your_id'  # Enter your gmail id
+
+gmail_password = 'your_password'  # Create an app password from accounts.google.com
+# Paste in here
+
+mysql_password = 'your_password'
+cloud_mysql_password = 'your_password'
+cricket_api = '2diNGBI3RZfmyVNWlePMvLSVr7m2'
